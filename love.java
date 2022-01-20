76b6d66fdce18d6b2a88d2bd07b12f7ea486d0ce
Nattawatt Hongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2022-01-20 08:34:16 +0700

    Create love.java

---
+++ love.java
@@ -0,0 +1,9 @@
+class love {
+    public static void main(String[] args) {
+        String Me="";
+        final String Yourheart = "Him";
+        if(Me=="Love U"){
+            Yourheart="Me";
+        }
+    }
+}
